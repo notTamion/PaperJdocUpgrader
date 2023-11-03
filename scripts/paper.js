e3252c5fa2bb7365ca0ee28019c31f2ecbbfe8fd
Tamion <70228790+notTamion@users.noreply.github.com>
2023-11-03 21:37:42 +0100

    Use replace instead so its not safed in the history

--- scripts/paper.js
+++ scripts/paper.js
@@ -1,5 +1,6 @@
-if (!window.location.href.startsWith("https://jd.papermc.io/paper/1.20")) {
-    let urlarray = window.location.href.replace("https://jd.papermc.io/paper/", "").split("/");
-    urlarray.shift();
-    window.location.href = "https://jd.papermc.io/paper/1.20/" + urlarray.join("/");
+if (window.location.href.startsWith("https://jd.papermc.io/paper/1.20")) {
+    return;
 }
+let urlarray = window.location.href.replace("https://jd.papermc.io/paper/", "").split("/");
+urlarray.shift();
+window.location.replace("https://jd.papermc.io/paper/1.20/" + urlarray.join("/"));
