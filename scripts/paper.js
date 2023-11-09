a684a25b44baca1117666219fa2f58fde62e19af
Tamion <70228790+notTamion@users.noreply.github.com>
2023-11-09 15:13:41 +0100

    Fix Error

--- scripts/paper.js
+++ scripts/paper.js
@@ -1,4 +1,3 @@
-if (window.location.href.startsWith("https://jd.papermc.io/paper/1.20")) {
-    return;
+if (!window.location.href.startsWith("https://jd.papermc.io/paper/1.20")) {
+    window.location.replace(window.location.href.replace(/[0-9]*\.[0-9]+/i, "1.20"));
 }
-window.location.replace(window.location.href.replace(/[0-9]*\.[0-9]+/i, "1.20"));
